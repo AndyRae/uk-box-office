6c422f03e1e509655f2218201d092ae79b3eed27
Andy Rae <hello@rae.li>
2022-01-24 20:32:11 +0000

    testing init

--- app/__init__.py
+++ app/__init__.py
@@ -1,81 +1,19 @@
-from typing import Any
+from flask import Blueprint, Flask
 
-from elasticsearch import Elasticsearch
-from flask import Flask
-from flask_apscheduler import APScheduler
-from flask_caching import Cache
-from flask_debugtoolbar import DebugToolbarExtension
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-scheduler = APScheduler()
-toolbar = DebugToolbarExtension()
-cache = Cache()
-limiter = Limiter(key_func=get_remote_address)
+bp = Blueprint("index", __name__, template_folder="templates")
 
 
-def create_app(test_config: Any = None) -> Flask:
+@bp.route("/")
+def index() -> str:
+    """
+    Main dashboard view - all of work is done in JS. See Template.
+    """
+    return "hello world!"
+
+
+def create_app() -> Flask:
     app = Flask(__name__, instance_relative_config=True)
 
-    from . import settings
-
-    if test_config is None:
-        # load the instance config - if it exists, when not testing
-        app.config.from_object(settings.DevelopmentConfig)
-
-        # Debug toolbar
-        toolbar.init_app(app)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    register_extensions(app)
-
-    # Elasticsearch
-    app.elasticsearch = (  # type: ignore
-        Elasticsearch(
-            [{"host": app.config["ELASTICSEARCH_URL"], "port": 9200}]
-        )
-        if app.config["ELASTICSEARCH_URL"]
-        else None
-    )
-
     with app.app_context():
-        register_blueprints(app)
-        register_cli(app)
-
-        scheduler.start()
-
+        app.register_blueprint(bp)
         return app
-
-
-def register_extensions(app: Flask) -> None:
-    from . import tasks
-
-    db.init_app(app)
-    scheduler.init_app(app)
-    cache.init_app(app)
-    limiter.init_app(app)
-    return None
-
-
-def register_blueprints(app: Flask) -> None:
-    from . import api, views
-
-    app.register_blueprint(api.bp)
-    app.register_blueprint(views.bp)
-    return None
-
-
-def register_cli(app: Flask) -> None:
-    from . import cli
-
-    app.cli.add_command(cli.init_db_command)
-    app.cli.add_command(cli.fill_db_command)
-    app.cli.add_command(cli.weekly_etl_command)
-    app.cli.add_command(cli.backup_etl)
-    app.cli.add_command(cli.rollback_etl_command)
-
-    return None
