d36510ee5f5bb5237588e32469f88f205b42c3c1
Andy Rae <hello@rae.li>
2022-02-02 21:21:50 +0000

    static

--- src/ukbo/__init__.py
+++ src/ukbo/__init__.py
@@ -5,12 +5,14 @@ from flask import Flask
 from flask_apscheduler import APScheduler
 from flask_caching import Cache
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_flatpages import FlatPages
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 scheduler = APScheduler()
+pages = FlatPages()
 toolbar = DebugToolbarExtension()
 cache = Cache()
 limiter = Limiter(key_func=get_remote_address)
@@ -57,6 +59,7 @@ def register_extensions(app: Flask) -> None:
 
     db.init_app(app)
     scheduler.init_app(app)
+    pages.init_app(app)
     cache.init_app(app)
     limiter.init_app(app)
     return None
---
+++ src/ukbo/pages/contact.md
@@ -0,0 +1,8 @@
+This is built by just one person - [Andy Rae](https://rae.li).
+
+You can email me if you think something is wrong, could be better, or want to talk.
+
+hello@rae.li
+
+
+[@AndyRae_](https://twitter.com/AndyRae_)
