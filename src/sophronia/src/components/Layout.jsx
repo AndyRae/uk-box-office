cb5d806db361d9667dd5b6a3ecec9d523c663087
Andy Rae <hello@rae.li>
2022-09-20 20:52:15 +0100

    Test precommit

--- src/sophronia/src/components/Layout.jsx
+++ src/sophronia/src/components/Layout.jsx
@@ -6,7 +6,7 @@ export const Layout = () => {
 		<div class="fixed flex w-full h-full">
 			<Sidebar />
 
-			<div class="flex-1 p-10 overflow-auto  bg-gray-50 dark:bg-gray-900  dark:text-white">
+			<div class="flex-1 p-10 overflow-auto  bg-gray-50 dark:bg-gray-900 dark:text-white">
         <Outlet />
 			</div>
 		</div>
