0b492cb4aee1bea36ddcde69ffe06f62012bcd43
Andy Rae <hello@rae.li>
2022-10-04 20:08:32 +0100

    Add further generic tables

--- src/sophronia/src/components/Country/CountryList.jsx
+++ src/sophronia/src/components/Country/CountryList.jsx
@@ -1,40 +1,23 @@
 import { Link } from 'react-router-dom';
+import { BaseTable, Td, Tr } from '../charts/BaseTable';
 
-export const CountryList = ({ countries, pageIndex }) => {
+export const CountryList = ({ countries }) => {
+	const columns = [{ label: 'Country' }];
+
 	return (
 		<div>
 			<h1 className='text-3xl font-bold py-5'>Countries</h1>
 
 			{countries && (
-				<div className='overflow-x-auto relative'>
-					<table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
-						<thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
-							<tr>
-								<th scope='col' className='py-3 px-6'>
-									Country
-								</th>
-							</tr>
-						</thead>
-						<tbody>
-							{countries &&
-								countries.results.map((country) => {
-									return (
-										<tr
-											key={country.id}
-											className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
-										>
-											<th
-												scope='row'
-												className='py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white'
-											>
-												<Link to={country.slug}>{country.name}</Link>
-											</th>
-										</tr>
-									);
-								})}
-						</tbody>
-					</table>
-				</div>
+				<BaseTable columns={columns}>
+					{countries.results.map((country, index) => (
+						<Tr key={country.id} index={index}>
+							<Td isHighlight>
+								<Link to={country.slug}>{country.name}</Link>
+							</Td>
+						</Tr>
+					))}
+				</BaseTable>
 			)}
 		</div>
 	);
--- src/sophronia/src/components/Distributor/DistributorList.jsx
+++ src/sophronia/src/components/Distributor/DistributorList.jsx
@@ -1,40 +1,22 @@
 import { Link } from 'react-router-dom';
+import { BaseTable, Td, Tr } from '../charts/BaseTable';
 
-export const DistributorList = ({ distributors, pageIndex }) => {
+export const DistributorList = ({ distributors }) => {
+	const columns = [{ label: 'Distributor' }];
+
 	return (
 		<div>
 			<h1 className='text-3xl font-bold py-5'>Distributors</h1>
-
 			{distributors && (
-				<div className='overflow-x-auto relative'>
-					<table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
-						<thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
-							<tr>
-								<th scope='col' className='py-3 px-6'>
-									Distributor
-								</th>
-							</tr>
-						</thead>
-						<tbody>
-							{distributors &&
-								distributors.results.map((distributor) => {
-									return (
-										<tr
-											key={distributor.id}
-											className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'
-										>
-											<th
-												scope='row'
-												className='py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white'
-											>
-												<Link to={distributor.slug}>{distributor.name}</Link>
-											</th>
-										</tr>
-									);
-								})}
-						</tbody>
-					</table>
-				</div>
+				<BaseTable columns={columns}>
+					{distributors.results.map((distributor, index) => (
+						<Tr key={distributor.id} index={index}>
+							<Td isHighlight>
+								<Link to={distributor.slug}>{distributor.name}</Link>
+							</Td>
+						</Tr>
+					))}
+				</BaseTable>
 			)}
 		</div>
 	);
