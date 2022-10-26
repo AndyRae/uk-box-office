f88c52b45d61b3e56d643c382b28717ab3ae9148
Andy Rae <hello@rae.li>
2022-10-26 19:20:01 +0100

    Add cumulative chart to film

--- src/sophronia/src/components/Film/FilmTimeChart.jsx
+++ src/sophronia/src/components/Film/FilmTimeChart.jsx
@@ -5,7 +5,7 @@ export const FilmTimeChart = ({ data }) => {
 		labels: data.map((d) => d.date),
 		datasets: [
 			{
-				label: 'Actual',
+				label: 'Weekly Box Office',
 				data: data.map((d) => d.week_gross),
 				fill: true,
 				backgroundColor: ['#FE7E6D4D'],
@@ -13,6 +13,16 @@ export const FilmTimeChart = ({ data }) => {
 				pointStyle: 'circle',
 				tension: 0.3,
 				yAxisID: 'y',
+			},
+			{
+				label: 'Cumulative',
+				data: data.map((d) => d.total_gross),
+				fill: true,
+				backgroundColor: ['#1E3A8A4D'],
+				borderColor: ['#1E3A8A'],
+				pointStyle: 'circle',
+				tension: 0.3,
+				yAxisID: 'y1',
 			},
 		],
 	};
@@ -33,6 +43,35 @@ export const FilmTimeChart = ({ data }) => {
 				grid: {
 					display: false,
 				},
+				position: 'left',
+				offset: false,
+				ticks: {
+					autoSkip: true,
+					stepSize: 10000000,
+					callback: function (value, index, values) {
+						var ranges = [
+							{ divider: 1e6, suffix: 'M' },
+							{ divider: 1e3, suffix: 'k' },
+						];
+						function formatNumber(n) {
+							for (var i = 0; i < ranges.length; i++) {
+								if (n >= ranges[i].divider) {
+									return (n / ranges[i].divider).toString() + ranges[i].suffix;
+								}
+							}
+							return n;
+						}
+						return '£' + formatNumber(value);
+					},
+				},
+			},
+			y1: {
+				beginAtZero: true,
+				min: 0,
+				grid: {
+					display: false,
+				},
+				position: 'right',
 				offset: false,
 				ticks: {
 					autoSkip: true,
@@ -57,7 +96,7 @@ export const FilmTimeChart = ({ data }) => {
 		},
 		plugins: {
 			legend: {
-				display: false,
+				display: true,
 			},
 		},
 	};
