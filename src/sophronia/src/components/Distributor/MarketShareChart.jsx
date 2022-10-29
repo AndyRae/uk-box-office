2d008240d056e7729fa36265ff200bfc1d967b0b
Andy Rae <hello@rae.li>
2022-10-29 18:39:07 +0100

    Updated distributor market share chart

--- src/sophronia/src/components/Distributor/MarketShareChart.jsx
+++ src/sophronia/src/components/Distributor/MarketShareChart.jsx
@@ -3,10 +3,20 @@ import { Timeseries } from '../charts/Timeseries';
 export const MarketShareChart = ({ data }) => {
 	const uniqueYears = [...new Set(data.map((d) => d.year))];
 
-	// need to reduce the data to a single object for each distributor
+	// the total for each year
+	const yearsTotal = data.reduce((acc, curr) => {
+		if (acc[curr.year]) {
+			acc[curr.year] += curr.gross;
+		} else {
+			acc[curr.year] = curr.gross;
+		}
+		return acc;
+	}, {});
+
+	// reduce the data to a single object for each distributor
 	const uniqueDistributors = [...new Set(data.map((d) => d.distributor.name))];
 
-	// need to reduce the data to a single array for each distributor
+	// reduce the data to a single array for each distributor
 	const reducedData = uniqueDistributors.map((distributor) => {
 		const distributorData = data.filter(
 			(d) => d.distributor.name === distributor
@@ -16,20 +26,51 @@ export const MarketShareChart = ({ data }) => {
 			const total = yearData.reduce((acc, curr) => acc + curr.gross, 0);
 			return {
 				year,
-				market_share: total,
+				marketShare: total,
+				marketPercentage: total / yearsTotal[year],
 			};
 		});
 		return {
 			distributor,
-			data: reduced,
+			years: reduced,
 		};
 	});
 
-	console.log(reducedData);
+	// order the data by market share (we only want the top 10)
+	reducedData.sort((a, b) => {
+		const aTotal = a.years.reduce((acc, curr) => acc + curr.marketShare, 0);
+		const bTotal = b.years.reduce((acc, curr) => acc + curr.marketShare, 0);
+		return bTotal - aTotal;
+	});
+
+	var colors = [
+		'#fe7e6d',
+		'#fc9b89',
+		'#f8b6a5',
+		'#f2cfc0',
+		'#ede7d8',
+		'#d8d3e8',
+		'#aeaae8',
+		'#7f83de',
+		'#4c61c6',
+		'#17439b',
+	];
+
+	// construct dataset object for each distributor
+	var graphData = reducedData.slice(0, 10).map((d) => {
+		var randomColor = colors.shift();
+		return {
+			label: d.distributor,
+			data: d.years.map((d) => d.marketPercentage).reverse(),
+			fill: true,
+			backgroundColor: randomColor,
+			borderColor: randomColor,
+		};
+	});
 
 	const d = {
 		labels: uniqueYears.reverse(),
-		datasets: [{}],
+		datasets: graphData,
 	};
 	const options = {
 		scales: {
