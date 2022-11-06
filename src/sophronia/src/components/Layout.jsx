279c298de91c71d86267444a0043e76e778c5070
Andy Rae <hello@rae.li>
2022-11-06 21:16:22 +0000

    Add base structured data

--- src/sophronia/src/components/Layout.jsx
+++ src/sophronia/src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Sidebar } from './ui/Sidebar';
 import { Footer } from './ui/Footer';
+import { StructuredSiteData } from './StructuredData';
 
 export const Layout = () => {
 	return (
 		<div className='fixed flex w-full h-full'>
+			<StructuredSiteData />
 			<Sidebar />
 
 			<div className='flex-1 p-10 overflow-auto bg-gray-50 dark:bg-gray-900 dark:text-white'>
