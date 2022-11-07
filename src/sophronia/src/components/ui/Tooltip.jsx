1ad5adee4a1e680f241ea9d26c6635a4b31b5db2
Andy Rae <hello@rae.li>
2022-11-07 21:26:18 +0000

    Add tooltip shadow

--- src/sophronia/src/components/ui/Tooltip.jsx
+++ src/sophronia/src/components/ui/Tooltip.jsx
@@ -1,7 +1,7 @@
 export const Tooltip = ({ children, text, ...props }) => {
 	return (
 		<div className='group' {...props}>
-			<span className='tooltip-text text-sm font-medium bg-slate-50 dark:bg-slate-900 dark:text-white p-1 -mt-12 -mr-10 rounded-lg hidden group-hover:block absolute text-center py-2 px-4 z-50'>
+			<span className='tooltip-text text-sm font-medium shadow-md bg-slate-50 dark:bg-slate-900 dark:text-white p-1 -mt-12 -mr-10 rounded-lg hidden group-hover:block absolute text-center py-2 px-4 z-50'>
 				{text}
 			</span>
 			{children}
