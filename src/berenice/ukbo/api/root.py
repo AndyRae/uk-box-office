7dceda4e9f99fe71966414711f9e3716f2c2cb28
Andy Rae <hello@rae.li>
2022-11-29 20:14:23 +0000

    Add api 200 page

---
+++ src/berenice/ukbo/api/root.py
@@ -0,0 +1,15 @@
+from typing import Tuple
+
+from flask import Blueprint
+from ukbo import cache
+
+root = Blueprint("root", __name__)
+
+
+@root.route("/")
+@cache.cached()
+def index() -> Tuple[str, int]:
+    """
+    Root endpoint.
+    """
+    return ("", 200)
