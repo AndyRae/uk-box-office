4ce6fc9b6edd321eba9d1978cff2315a1b4eff02
Andy Rae <hello@rae.li>
2022-12-07 21:17:35 +0000

    Add utils documentation

--- src/sophronia/src/utils/scrollToTop.js
+++ src/sophronia/src/utils/scrollToTop.js
@@ -1,6 +1,12 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
+/**
+ * Scroll to top on route change
+ * @returns {null}
+ * @example
+ * <ScrollToTop />
+ */
 export default function ScrollToTop() {
 	const { pathname } = useLocation();
 
