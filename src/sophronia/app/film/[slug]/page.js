35c839c06e0a7538624b34d55956cb4c399d6427
Andy Rae <hello@rae.li>
2023-01-07 21:14:19 +0000

    Add film fetching

--- src/sophronia/app/film/[slug]/page.js
+++ src/sophronia/app/film/[slug]/page.js
@@ -1,3 +1,35 @@
+'use client';
+
+import { useFilm } from 'lib/films';
+import { useEffect } from 'react';
+
 export default function Page({ params }) {
-	return <div>Film: {params.slug}</div>;
+	const { data, error } = useFilm(params.slug);
+
+	// Unwrap first week date logic
+	const weekOne = data.weeks[0];
+	const weeksOnRelease = weekOne.weeks_on_release;
+	const isFirstWeek = weeksOnRelease === 1 ? true : false;
+	const releaseDate = weekOne.date;
+
+	const multiple = (data.gross / weekOne.weekend_gross).toFixed(2);
+
+	// Set the page title here as the data is fetched
+	useEffect(() => {
+		document.title = `${data?.name} - UK Box Office Data`;
+	}, []);
+
+	// Rename data to make it easy to reuse charts
+	const cumulativeData = data.weeks.map(
+		({ total_gross: week_gross, date }) => ({
+			date,
+			week_gross,
+		})
+	);
+
+	return (
+		<div>
+			Film: {params.slug} {data.name}: {multiple}
+		</div>
+	);
 }
--- src/sophronia/app/film/page.js
+++ src/sophronia/app/film/page.js
@@ -1,3 +1,14 @@
+'use client';
+
+import { useFilmList } from 'lib/films';
+import { useState } from 'react';
+import { paginate } from 'lib/utils/pagination';
+
 export default function Page() {
-	return <div>Holding Page</div>;
+	const [pageIndex, setPageIndex] = useState(1);
+	const pageLimit = 15;
+	const { data, error } = useFilmList(pageIndex, pageLimit);
+	const pageNumbers = paginate(data?.count, pageIndex, pageLimit);
+
+	return <div>Holding Page {pageNumbers}</div>;
 }
