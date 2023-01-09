d1ee98e346a6bcdefa223ef4c02029f3e92e9c8f
Andy Rae <hello@rae.li>
2023-01-09 20:57:51 +0000

    Add simple head files

---
+++ src/sophronia/app/forecast/head.tsx
@@ -0,0 +1,7 @@
+export default async function Head() {
+	return (
+		<>
+			<title>Forecast | Box Office Data</title>
+		</>
+	);
+}
---
+++ src/sophronia/app/marketshare/head.tsx
@@ -0,0 +1,7 @@
+export default async function Head() {
+	return (
+		<>
+			<title>Market Share | Box Office Data</title>
+		</>
+	);
+}
