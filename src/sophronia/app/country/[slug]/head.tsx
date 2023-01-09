2314d7db58606b89c74cc7038d92a40c7e1cbee1
Andy Rae <hello@rae.li>
2023-01-09 20:59:03 +0000

    Add country head

---
+++ src/sophronia/app/country/[slug]/head.tsx
@@ -0,0 +1,11 @@
+import { getCountry } from './page';
+
+export default async function Head({ params }: { params: { slug: string } }) {
+	const country = await getCountry(params.slug);
+
+	return (
+		<>
+			<title>{`${country.name} | Box Office Data`}</title>
+		</>
+	);
+}
