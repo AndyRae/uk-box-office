ccb04f85e40ed81e7a421e2fc545607ce83fc597
Andy Rae <hello@rae.li>
2023-01-19 19:55:55 +0000

    Add error detail

--- src/sophronia/app/error.tsx
+++ src/sophronia/app/error.tsx
@@ -16,8 +16,22 @@ export default function Error({
 
 	return (
 		<div>
-			<p>Something went wrong!</p>
-			<button onClick={() => reset()}>Reset error boundary</button>
+			<h1 className='text-4xl font-bold py-5 capitalize'>
+				An error has occurred.
+			</h1>
+			<div
+				className='flex p-4 mb-4 text-sm max-w-lg text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800'
+				role='alert'
+			>
+				<span className='sr-only'>Danger</span>
+				<div>
+					<span className='font-medium pb-5'>Sorry, something went wrong!</span>
+					<p>
+						Please try refreshing again in a moment, or try a different page.
+					</p>
+				</div>
+			</div>
+			<button onClick={() => reset()}>Reset.</button>
 		</div>
 	);
 }
