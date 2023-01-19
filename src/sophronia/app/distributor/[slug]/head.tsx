e8ee424879089b9c8bbc4f36e04309dd622e4823
Andy Rae <hello@rae.li>
2023-01-19 21:28:54 +0000

    Add slug based descriptions

--- src/sophronia/app/distributor/[slug]/head.tsx
+++ src/sophronia/app/distributor/[slug]/head.tsx
@@ -3,9 +3,14 @@ import { getDistributor } from './getDistributor';
 export default async function Head({ params }: { params: { slug: string } }) {
 	const distributor = await getDistributor(params.slug);
 
+	const description = `Get ${distributor.name} distributed films data at the UK Box Office.`;
+
 	return (
 		<>
 			<title>{`${distributor.name} | Box Office Data`}</title>
+			<meta name='description' content={description} />
+			<meta property='og:description' content={description} />
+			<meta name='twitter:description' content={description} />
 		</>
 	);
 }
--- src/sophronia/app/film/[slug]/head.tsx
+++ src/sophronia/app/film/[slug]/head.tsx
@@ -2,12 +2,18 @@ import { getFilm } from './getFilm';
 
 export default async function Head({ params }: { params: { slug: string } }) {
 	const film = await getFilm(params.slug);
+	const year = film.weeks[0].date.split('-')[0];
 
-	const title = `${film.name} | Box Office Data`;
+	const description = `${film.name} (${year}) was released in the UK on ${
+		film.weeks[0].date
+	}, and grossed £${film.gross.toLocaleString()} at the UK Box Office.`;
 
 	return (
 		<>
-			<title>{title}</title>
+			<title>{`${film.name} ${year} | Box Office Data`}</title>
+			<meta name='description' content={description} />
+			<meta property='og:description' content={description} />
+			<meta name='twitter:description' content={description} />
 		</>
 	);
 }
