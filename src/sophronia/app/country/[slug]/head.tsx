23df86308e567f0a529cbe4904bef9cdece21d8d
Andy Rae <hello@rae.li>
2023-01-22 19:38:43 +0000

    Fix async headers bug temp

--- src/sophronia/app/country/[slug]/head.tsx
+++ src/sophronia/app/country/[slug]/head.tsx
@@ -1,16 +1,28 @@
+import DefaultTags from '../../DefaultTags';
 import { getCountry } from './getCountry';
 
+// Remove async headers as broken in Next 13.1.1
+// See: https://github.com/vercel/next.js/issues/43972
+// export default async function Head({ params }: { params: { slug: string } }) {
+// 	const country = await getCountry(params.slug);
+
+// 	const description = `Get ${country.name} released films data at the UK Box Office.`;
+
+// 	return (
+// 		<>
+// 			<title>{`${country.name} | Box Office Data`}</title>
+// 			<meta name='description' content={description} />
+// 			<meta property='og:description' content={description} />
+// 			<meta name='twitter:description' content={description} />
+// 		</>
+// 	);
+// }
+
 export default async function Head({ params }: { params: { slug: string } }) {
-	const country = await getCountry(params.slug);
-
-	const description = `Get ${country.name} released films data at the UK Box Office.`;
-
 	return (
 		<>
-			<title>{`${country.name} | Box Office Data`}</title>
-			<meta name='description' content={description} />
-			<meta property='og:description' content={description} />
-			<meta name='twitter:description' content={description} />
+			<title>Countries | Box Office Data</title>
+			<DefaultTags />
 		</>
 	);
 }
--- src/sophronia/app/distributor/[slug]/head.tsx
+++ src/sophronia/app/distributor/[slug]/head.tsx
@@ -1,15 +1,26 @@
-import { getDistributor } from './getDistributor';
+// import { getDistributor } from './getDistributor';
 import DefaultTags from '../../DefaultTags';
 
+// Remove async headers as broken in Next 13.1.1
+// See: https://github.com/vercel/next.js/issues/43972
+// export default async function Head({ params }: { params: { slug: string } }) {
+// 	const distributor = await getDistributor(params.slug);
+
+// 	const description = `Get ${distributor.name} distributed films data at the UK Box Office.`;
+
+// 	return (
+// 		<>
+// 			<title>{`${distributor.name} | Box Office Data`}</title>
+// 			<DefaultTags description={description} />
+// 		</>
+// 	);
+// }
+
 export default async function Head({ params }: { params: { slug: string } }) {
-	const distributor = await getDistributor(params.slug);
-
-	const description = `Get ${distributor.name} distributed films data at the UK Box Office.`;
-
 	return (
 		<>
-			<title>{`${distributor.name} | Box Office Data`}</title>
-			<DefaultTags description={description} />
+			<title>Distributors | Box Office Data</title>
+			<DefaultTags />
 		</>
 	);
 }
