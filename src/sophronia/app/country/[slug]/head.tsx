a8fdfa14b2710d9d2a86ff131bbb571c48ddfa8d
Andy Rae <hello@rae.li>
2023-01-22 02:34:44 +0000

    Test head.tsx loading bug

--- src/sophronia/app/country/[slug]/head.tsx
+++ src/sophronia/app/country/[slug]/head.tsx
@@ -1,16 +1,17 @@
-import { getCountry } from './getCountry';
+// import { getCountry } from './getCountry';
 
 export default async function Head({ params }: { params: { slug: string } }) {
-	const country = await getCountry(params.slug);
+	// const country = await getCountry(params.slug);
 
-	const description = `Get ${country.name} released films data at the UK Box Office.`;
+	// const description = `Get ${country.name} released films data at the UK Box Office.`;
 
 	return (
 		<>
-			<title>{`${country.name} | Box Office Data`}</title>
+			<title>{`Box Office Data`}</title>
+			{/* <title>{`${country.name} | Box Office Data`}</title>
 			<meta name='description' content={description} />
 			<meta property='og:description' content={description} />
-			<meta name='twitter:description' content={description} />
+			<meta name='twitter:description' content={description} /> */}
 		</>
 	);
 }
