0b5885b7c7a8dfa5d72e04e4b6ca2b2d3c6974f0
Andy Rae <hello@rae.li>
2023-02-12 19:22:57 +0000

    Add migration

--- src/berenice/ukbo/models/__init__.py
+++ src/berenice/ukbo/models/__init__.py
@@ -9,6 +9,7 @@ The models are used to create the database tables, and to query the database.
 from . import models
 from .Country import Country
 from .Distributor import Distributor
+from .Event import Event
 from .Film import Film
 from .Film_Week import Film_Week
 from .Week import Week
