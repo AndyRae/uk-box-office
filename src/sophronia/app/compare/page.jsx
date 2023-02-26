7659781b06279cd7caeab82c9183b5f8229ac23a
Andy Rae <hello@rae.li>
2023-02-26 19:07:06 +0000

    Working film data

--- src/sophronia/app/compare/page.jsx
+++ src/sophronia/app/compare/page.jsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { useState, setState } from 'react';
+import { useState, setState, useEffect } from 'react';
 import { PageTitle } from 'components/ui/PageTitle';
 import { getFilm } from 'app/film/[slug]/getFilm';
 import { getBackendURL } from 'lib/ApiFetcher';
 import AsyncSelect from 'react-select/async';
+import { Grid } from 'components/ui/Grid';
+import { Card } from 'components/ui/Card';
 
 async function SearchFilms(term) {
 	const url = getBackendURL();
@@ -15,7 +17,10 @@ async function SearchFilms(term) {
 async function FilmsToOptions(term) {
 	const results = await SearchFilms(term);
 
-	const parsed = results.map((film) => ({ value: film.id, label: film.name }));
+	const parsed = results.map((film) => ({
+		value: film.slug,
+		label: film.name,
+	}));
 	return parsed;
 }
 
@@ -23,20 +28,39 @@ const promiseOptions = (inputValue) =>
 	new Promise((resolve) => {
 		setTimeout(() => {
 			resolve(FilmsToOptions(inputValue));
-		}, 1000);
+		}, 100);
 	});
 
 export default function Page() {
 	// array of film ids
+	const [selectedFilms, setSelectedFilms] = useState(null);
 
-	// try to just load one film in.
+	// array of complete film objects
+	const [filmData, setFilmData] = useState();
+
+	// get film data when added/removed
+	useEffect(() => {
+		let filmData = [];
+		selectedFilms?.forEach(async (element) => {
+			const data = await getFilm(element.value);
+			filmData.push(data);
+		});
+
+		setFilmData(filmData);
+	}, [selectedFilms]);
+
+	// compare box office - table?
 
 	return (
 		<>
 			<PageTitle>Compare Films</PageTitle>
-			{/* <SearchFilm /> */}
-			<AsyncSelect isMulti cacheOptions loadOptions={promiseOptions} />
-			{/* Typeahead box */}
+			<AsyncSelect
+				isMulti
+				cacheOptions
+				loadOptions={promiseOptions}
+				onChange={setSelectedFilms}
+			/>
+			<Grid></Grid>
 		</>
 	);
 }
