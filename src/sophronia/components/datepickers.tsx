a9e8111f9308dbcdadbea2107d75384ecb8c6c85
Andy Rae <hello@rae.li>
2023-04-12 23:55:51 +0100

    Update datepickers

--- src/sophronia/components/datepickers.tsx
+++ src/sophronia/components/datepickers.tsx
@@ -1,13 +1,17 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { parseISO } from 'date-fns';
+
 import DatePicker from 'react-datepicker';
+import { useState } from 'react';
 
 import 'react-datepicker/dist/react-datepicker.css';
+import { parseDate } from 'lib/utils/dates';
 
 type DatepickersProps = {
-	start: Date;
-	end: Date;
-	setStart: (date: Date) => void;
-	setEnd: (date: Date) => void;
-	minimum: Date;
+	startParam: string;
+	endParam: string;
 };
 
 /**
@@ -21,12 +25,34 @@ type DatepickersProps = {
  * @returns {JSX.Element}
  */
 export const Datepickers = ({
-	start,
-	end,
-	setStart,
-	setEnd,
-	minimum,
+	startParam,
+	endParam,
 }: DatepickersProps): JSX.Element => {
+	Date.prototype.addDays = function (days) {
+		var date = new Date(this.valueOf());
+		date.setDate(date.getDate() + days);
+		return date;
+	};
+
+	const router = useRouter();
+
+	const daysToShow = 90;
+	const daysAllowedToGoBack = 547; // 547 days is 18 months
+	const s = new Date();
+
+	const [start, setStart] = useState(s.addDays(-daysToShow));
+	const [end, setEnd] = useState(new Date());
+
+	const handleStart = (date: any) => {
+		setStart(date!);
+		router.push(`?s=${parseDate(date)}&e=${parseDate(end)}`);
+	};
+
+	const handleEnd = (date: any) => {
+		setEnd(date!);
+		router.push(`?s=${parseDate(start)}&e=${parseDate(date)}`);
+	};
+
 	return (
 		<div>
 			<div className='grid grid-cols-2 gap-2 items-center'>
@@ -34,23 +60,23 @@ export const Datepickers = ({
 					<DatePicker
 						className='w-full text-center py-1.5 overflow-hidden text-sm font-medium bg-white dark:bg-black rounded-lg hover:bg-gray-100 dark:hover:text-black text-gray-900 dark:text-white focus:outline-none border-2 border-gray-900 dark:border-white transition-all duration-150'
 						dateFormat='dd/MM/yyyy'
-						selected={start}
-						onChange={(date) => setStart(date!)}
+						selected={parseISO(startParam)}
+						onChange={(date) => handleStart(date!)}
 						selectsStart
 						startDate={start}
 						endDate={end}
 						filterDate={(d) => {
 							return new Date() > d;
 						}}
-						minDate={minimum}
+						minDate={s.addDays(-daysAllowedToGoBack)}
 					/>
 				</div>
 				<div className=''>
 					<DatePicker
 						className='w-full text-center py-1.5 overflow-hidden text-sm font-medium bg-white dark:bg-black rounded-lg hover:bg-gray-100 dark:hover:text-black text-gray-900 dark:text-white focus:outline-none border-2 border-gray-900 dark:border-white transition-all duration-150'
 						dateFormat='dd/MM/yyyy'
-						selected={end}
-						onChange={(date) => setEnd(date!)}
+						selected={parseISO(endParam)}
+						onChange={(date) => handleEnd(date!)}
 						selectsEnd
 						startDate={start}
 						endDate={end}
