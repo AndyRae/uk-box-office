f015acd2ec781875b7fcee1b9d361561ab84081b
Andy Rae <hello@rae.li>
2023-04-12 23:56:04 +0100

    Move fetch ssr

--- src/sophronia/app/page.tsx
+++ src/sophronia/app/page.tsx
@@ -1,10 +1,10 @@
-'use client';
+// 'use client';
 
-import { useState, useEffect } from 'react';
+import { parseISO } from 'date-fns';
 import {
-	useBoxOfficeInfinite,
-	useBoxOfficePreviousYear,
-} from 'lib/fetch/boxoffice';
+	fetchBoxOfficeInfinite,
+	fetchBoxOfficePreviousYear,
+} from 'lib/fetch/box';
 import { Button } from 'components/ui/button';
 import { ButtonGroup } from 'components/ui/button-group';
 import { Card } from 'components/ui/card';
@@ -29,44 +29,35 @@ import { LastUpdated } from 'components/last-updated';
 import { StackedBarChart } from 'components/charts/stacked-bar';
 import { DatasourceCard } from 'components/datasource';
 import { ChartWrapper } from 'components/charts/chart-wrapper';
+import { parseDate } from 'lib/utils/dates';
 
-export default function Home(): JSX.Element {
+export default async function Home({
+	searchParams,
+}: {
+	searchParams: { s?: string; e?: string };
+}): Promise<JSX.Element> {
 	Date.prototype.addDays = function (days) {
 		var date = new Date(this.valueOf());
 		date.setDate(date.getDate() + days);
 		return date;
 	};
 
-	// Parse dates to YYYY-MM-DD for the API
-	const parseDate = (input: Date): string => {
-		return `${input.getFullYear()}-${input.getMonth() + 1}-${input.getDate()}`;
-	};
+	// Calculate defaults at 90 days.
+	const s = parseDate(new Date().addDays(-90));
+	const e = parseDate(new Date());
 
-	// Set the default dates
-	const daysToShow = 90;
-	const daysAllowedToGoBack = 547; // 547 days is 18 months
-	const s = new Date();
-	const [start, setStart] = useState(s.addDays(-daysToShow));
-	const [end, setEnd] = useState(new Date());
-
-	// Parse dates for the API
-	const [startDate, setStartDate] = useState(parseDate(start));
-	const [endDate, setEndDate] = useState(parseDate(end));
-
-	useEffect(() => {
-		setStartDate(parseDate(start));
-		setEndDate(parseDate(end));
-	}, [start, end]);
+	// Get dates from the searchparams.
+	const start = searchParams?.s ?? s;
+	const end = searchParams?.e ?? e;
 
 	// Fetch data from the API
-	const { results, mutate, error, isReachedEnd } = useBoxOfficeInfinite(
-		startDate,
-		endDate
+	const { results, isReachedEnd, percentFetched } =
+		await fetchBoxOfficeInfinite(start, end);
+	const { data: timeComparisonData } = await fetchBoxOfficePreviousYear(
+		start,
+		end
 	);
-	const { data: timeComparisonData } = useBoxOfficePreviousYear(
-		startDate,
-		endDate
-	);
+	console.log(timeComparisonData);
 
 	// Group Data for the charts
 	const tableData = groupForTable(results);
@@ -87,38 +78,39 @@ export default function Home(): JSX.Element {
 	let changeWeek = 0;
 	let changeCinemas = 0;
 
-	if (timeComparisonData!.results.length >= 1) {
-		const lastYear = timeComparisonData!.results[0];
+	// if (timeComparisonData!.results.length >= 1) {
+	// 	const lastYear = timeComparisonData!.results[0];
 
-		changeNewFilms = Math.ceil(
-			((numberOfNewFilms - lastYear.number_of_releases) /
-				lastYear.number_of_releases) *
-				100
-		);
-		changeWeek = Math.ceil(
-			((boxOffice - lastYear.week_gross) / lastYear.week_gross) * 100
-		);
-		changeWeekend = Math.ceil(
-			((weekendBoxOffice - lastYear.weekend_gross) / lastYear.weekend_gross) *
-				100
-		);
-		changeCinemas = Math.ceil(
-			((numberOfCinemas - lastYear.number_of_cinemas) /
-				lastYear.number_of_cinemas) *
-				100
-		);
-	}
+	// 	changeNewFilms = Math.ceil(
+	// 		((numberOfNewFilms - lastYear.number_of_releases) /
+	// 			lastYear.number_of_releases) *
+	// 			100
+	// 	);
+	// 	changeWeek = Math.ceil(
+	// 		((boxOffice - lastYear.week_gross) / lastYear.week_gross) * 100
+	// 	);
+	// 	changeWeekend = Math.ceil(
+	// 		((weekendBoxOffice - lastYear.weekend_gross) / lastYear.weekend_gross) *
+	// 			100
+	// 	);
+	// 	changeCinemas = Math.ceil(
+	// 		((numberOfCinemas - lastYear.number_of_cinemas) /
+	// 			lastYear.number_of_cinemas) *
+	// 			100
+	// 	);
+	// }
 
 	// Buttons for the date picker
 	const changeDate = async (days: number) => {
 		const today = new Date();
-		setStart(today.addDays(-days));
+		// setStart(today.addDays(-days));
 	};
 
 	// Work out the difference between the last two dates
 	// Used to set the active button
-	const diffTime = Math.abs(s.getTime() - start.getTime());
-	const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+	// const diffTime = Math.abs(s.getTime() - start.getTime());
+	// const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+	const diffDays = 7;
 
 	return (
 		<div className='transition ease-in-out'>
@@ -130,15 +122,9 @@ export default function Home(): JSX.Element {
 
 			{/* Controls */}
 			<div className='flex flex-wrap mb-2 gap-y-4 items-center justify-center'>
-				<Datepickers
-					start={start}
-					end={end}
-					setStart={setStart}
-					setEnd={setEnd}
-					minimum={s.addDays(-daysAllowedToGoBack)}
-				/>
+				<Datepickers startParam={start} endParam={end} />
 				<ButtonGroup>
-					<Button onClick={() => changeDate(7)} isActive={diffDays === 7}>
+					{/* <Button onClick={() => changeDate(7)} isActive={diffDays === 7}>
 						1W
 					</Button>
 					<Button onClick={() => changeDate(30)} isActive={diffDays === 30}>
@@ -149,7 +135,7 @@ export default function Home(): JSX.Element {
 					</Button>
 					<Button onClick={() => changeDate(365)} isActive={diffDays === 365}>
 						1Y
-					</Button>
+					</Button> */}
 				</ButtonGroup>
 
 				<LastUpdated date={lastUpdated} />
