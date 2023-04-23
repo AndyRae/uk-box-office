14ff463ee87a8906afc9dbbf5e9bcbe40b7670ae
Andy Rae <hello@rae.li>
2023-04-23 16:30:07 +0100

    Update calendar

--- src/sophronia/components/ui/calendar.tsx
+++ src/sophronia/components/ui/calendar.tsx
@@ -5,7 +5,7 @@ import clsx from 'clsx';
 import { DayPicker } from 'react-day-picker';
 
 import { Icons } from 'components/icons';
-import { Button } from 'components/ui/button';
+import { buttonVariants } from 'components/ui/button-new';
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
@@ -27,10 +27,10 @@ function Calendar({
 				caption: 'flex justify-center pt-1 relative items-center',
 				caption_label: 'text-sm font-medium',
 				nav: 'space-x-1 flex items-center',
-				// nav_button: clsx(
-				//   Button(),
-				//   "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
-				// ),
+				nav_button: clsx(
+					buttonVariants({ variant: 'outline' }),
+					'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100'
+				),
 				nav_button_previous: 'absolute left-1',
 				nav_button_next: 'absolute right-1',
 				table: 'w-full border-collapse space-y-1',
@@ -39,10 +39,10 @@ function Calendar({
 					'text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]',
 				row: 'flex w-full mt-2',
 				cell: 'text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
-				// day: clsx(
-				//   buttonVariants({ variant: "ghost" }),
-				//   "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
-				// ),
+				day: clsx(
+					buttonVariants({ variant: 'ghost' }),
+					'h-9 w-9 p-0 font-normal aria-selected:opacity-100'
+				),
 				day_selected:
 					'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
 				day_today: 'bg-accent text-accent-foreground',
