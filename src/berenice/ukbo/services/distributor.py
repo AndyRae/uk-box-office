b6f732b9aa3d6cfd285798a077b84254bdf7a5ed
Andy Rae <hello@rae.li>
2023-05-07 20:37:33 +0100

    Change api order

--- src/berenice/ukbo/services/distributor.py
+++ src/berenice/ukbo/services/distributor.py
@@ -83,7 +83,7 @@ def get_films(slug: str, page: int = 1, limit: int = 100) -> Response:
     query = query.join(models.Distributor)
 
     query = query.filter(models.Distributor.slug == slug)
-    data = query.order_by(models.Film.id.asc()).paginate(
+    data = query.order_by(models.Film.id.desc()).paginate(
         page=page, per_page=limit, error_out=False
     )
 
