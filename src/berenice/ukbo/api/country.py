6c565343c7f93c97eaea49f9af0ee77de102b6b1
Andy Rae <hello@rae.li>
2023-05-08 16:06:25 +0100

    Add api endpoint

--- src/berenice/ukbo/api/country.py
+++ src/berenice/ukbo/api/country.py
@@ -57,3 +57,22 @@ def get_films(slug: str) -> Response:
     page = int(request.args.get("page", 1))
     limit = int(request.args.get("limit", 100))
     return services.country.get_films(slug, page, limit)
+
+
+@country.route("/<slug>/boxoffice", methods=["GET"])
+def get_box_office(slug: str) -> Response:
+    """
+    Get a countries box office history grouped by year.
+
+    Arguments:
+        slug: Country slug to return.
+
+    Request Arguments (optional):
+        limit: Number of years to backwards.
+
+    Returns:
+        JSON response of a countries box office history.
+    """
+    limit = request.args.get("limit", 1)
+
+    return services.country.get_box_office(slug, int(limit))
