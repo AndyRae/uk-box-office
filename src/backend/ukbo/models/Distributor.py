2cd4637bf894964aa5889de475ff5d852de26aa0
Andy Rae <hello@rae.li>
2023-07-16 19:47:15 +0100

    Update dist model

--- src/backend/ukbo/models/Distributor.py
+++ src/backend/ukbo/models/Distributor.py
@@ -14,13 +14,11 @@ class Distributor(PkModel):  # type: ignore
     Attributes:
         name: Name of the distributor.
         slug: Slug of the distributor.
-        weeks: List of Film weeks that the distributor has released films in.
 
     """
 
     __tablename__ = "distributor"
     name = db.Column(db.String(160), unique=True, nullable=False)
-    weeks = db.relationship("Film_Week", backref="distributor", lazy="dynamic")
     slug = db.Column(db.String(160), nullable=False, unique=True)
 
     def __init__(self, *args: str, **kwargs: str) -> None:
