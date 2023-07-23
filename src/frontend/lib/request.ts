6bad126b5d456853eefd65fc22c854fd5a8d9d4d
Andy Rae <hello@rae.li>
2023-07-23 19:38:28 +0100

    Add requests fetch

---
+++ src/frontend/lib/request.ts
@@ -0,0 +1,41 @@
+// Error class for API errors
+class APIError extends Error {
+	constructor(message: string, status: number) {
+		super(message);
+		this.name = 'APIError';
+	}
+}
+
+interface RequestOptions {
+	method?: string;
+	headers?: HeadersInit;
+	body?: BodyInit;
+}
+
+const defaultHeaders: HeadersInit = {
+	'Content-Type': 'application/json',
+};
+
+const request = async <T>(
+	url: string,
+	options: RequestOptions = {}
+): Promise<T> => {
+	try {
+		const response = await fetch(url, {
+			method: options.method || 'GET',
+			headers: { ...defaultHeaders, ...options.headers },
+			body: options.body,
+		});
+
+		if (!response.ok) {
+			const errorMessage = await response.text();
+			throw new APIError(errorMessage, response.status);
+		}
+
+		return response.json();
+	} catch (error) {
+		throw new APIError('Failed to fetch data', 500);
+	}
+};
+
+export default request;
