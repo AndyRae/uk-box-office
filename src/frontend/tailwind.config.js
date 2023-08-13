cbd2bfd8c5e45f684b13b9eb9fdcffa78ee27cc8
Andy Rae <hello@rae.li>
2023-08-13 21:25:48 +0100

    Remove css variables

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
 		'./src/**/*.{ts,tsx}',
 	],
 	theme: {
+		colors: {
+			'bo-black': '#070707',
+			'bo-primary': '#10b981', // green
+			'bo-metric-green': '#1A6622',
+		},
 		container: {
 			center: true,
 			padding: '2rem',
@@ -16,49 +21,6 @@ module.exports = {
 			},
 		},
 		extend: {
-			colors: {
-				'bo-black': '#070707',
-				'bo-primary': '#10b981', // green
-				'bo-metric-green': '#1A6622',
-				border: 'hsl(var(--border))',
-				input: 'hsl(var(--input))',
-				ring: 'hsl(var(--ring))',
-				background: 'hsl(var(--background))',
-				foreground: 'hsl(var(--foreground))',
-				primary: {
-					DEFAULT: 'hsl(var(--primary))',
-					foreground: 'hsl(var(--primary-foreground))',
-				},
-				secondary: {
-					DEFAULT: 'hsl(var(--secondary))',
-					foreground: 'hsl(var(--secondary-foreground))',
-				},
-				destructive: {
-					DEFAULT: 'hsl(var(--destructive))',
-					foreground: 'hsl(var(--destructive-foreground))',
-				},
-				muted: {
-					DEFAULT: 'hsl(var(--muted))',
-					foreground: 'hsl(var(--muted-foreground))',
-				},
-				accent: {
-					DEFAULT: 'hsl(var(--accent))',
-					foreground: 'hsl(var(--accent-foreground))',
-				},
-				popover: {
-					DEFAULT: 'hsl(var(--popover))',
-					foreground: 'hsl(var(--popover-foreground))',
-				},
-				card: {
-					DEFAULT: 'hsl(var(--card))',
-					foreground: 'hsl(var(--card-foreground))',
-				},
-			},
-			borderRadius: {
-				lg: 'var(--radius)',
-				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)',
-			},
 			keyframes: {
 				'accordion-down': {
 					from: { height: 0 },
