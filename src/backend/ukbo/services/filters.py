02a212e41d705015e63a344853e87256df4473ed
Andy Rae <hello@rae.li>
2023-10-08 14:49:10 +0100

    Move filter logic to class

--- src/backend/ukbo/services/filters.py
+++ src/backend/ukbo/services/filters.py
@@ -2,7 +2,7 @@ from typing import List, Optional
 
 import flask_sqlalchemy
 from flask import jsonify
-from sqlalchemy import func
+from sqlalchemy import extract, func
 from ukbo import models
 
 
@@ -92,3 +92,70 @@ class QueryFilter:
         self.min_box = min_box
         self.max_box = max_box
         self.sort = sort
+
+    def add_filter(
+        self, query: flask_sqlalchemy.query.Query
+    ) -> flask_sqlalchemy.query.Query:
+        """
+        Adds filters and sorting to a query if they are present.
+
+        Args:
+            query: The query that will be executed.
+
+        Returns: The query with filters applied.
+        """
+        if self.distributor_ids is not None:
+            query = query.join(models.distributors).join(models.Distributor)
+            query = query.filter(
+                models.Distributor.id.in_(self.distributor_ids)
+            )
+
+        if self.country_ids is not None:
+            query = query.join(models.countries).join(models.Country)
+            query = query.filter(models.Country.id.in_(self.country_ids))
+
+        if (
+            self.min_year is not None
+            or self.max_year is not None
+            or self.min_box is not None
+            or self.max_box is not None
+            or self.sort is not None
+        ):
+            query = query.join(models.Film_Week).group_by(models.Film.id)
+
+        if self.min_year is not None:
+            query = query.filter(
+                extract("year", models.Film_Week.date) >= self.min_year
+            )
+
+        if self.max_year is not None:
+            query = query.filter(
+                extract("year", models.Film_Week.date) <= self.max_year
+            )
+
+        if self.min_box is not None:
+            query = query.having(
+                func.max(models.Film_Week.total_gross) >= self.min_box
+            )
+
+        if self.max_box is not None:
+            query = query.having(
+                func.max(models.Film_Week.total_gross) <= self.max_box
+            )
+
+        # TODO: Remove this and apply the sorting filter.
+        # Apply sorting
+        # Define the sorting options and their corresponding ordering expressions
+        sorting_options = {
+            "asc_name": models.Film.name.asc(),
+            "desc_name": models.Film.name.desc(),
+            "asc_box": func.max(models.Film_Week.total_gross).asc(),
+            "desc_box": func.max(models.Film_Week.total_gross).desc(),
+        }
+
+        if self.sort is not None:
+            sort_option = sorting_options.get(self.sort)
+            if sort_option is not None:
+                query = query.order_by(sort_option)
+
+        return query
